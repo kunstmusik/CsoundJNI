a60a4a62e056fa38c46160c41f5564d2b8fadacf
Steven Yi <stevenyi@gmail.com>
2020-04-26 22:04:04 -0400

    initial running version

--- src/JCsound.cpp
+++ src/JCsound.cpp
@@ -9,21 +9,384 @@
  * Signature: (I)J
  */
 JNIEXPORT jlong JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundCreate
-  (JNIEnv *env, jobject obj, jint args) {
-    
-     CSOUND* csound = csoundCreate(NULL); 
-     return (jlong)csound;
+(JNIEnv *env, jclass clazz) {
+  CSOUND* csound = csoundCreate(NULL); 
+  return (jlong)csound;
+}
+
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundGetVersion
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundGetVersion
+(JNIEnv *env, jclass clazz) {
+  return csoundGetVersion();
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundSetOption
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundSetOption
+(JNIEnv *env, jclass clazz, jlong csoundPtr, jstring option) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+  const char *nativeString = env->GetStringUTFChars(option, 0);
+
+  jint retVal = csoundSetOption(csound, nativeString);
+
+  env->ReleaseStringUTFChars(option, nativeString);
+
+  return retVal;
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundEvalCode
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundEvalCode
+(JNIEnv *env, jclass clazz, jlong csoundPtr, jstring code) {
+
+  CSOUND* csound = (CSOUND*)csoundPtr;
+  const char *nativeString = env->GetStringUTFChars(code, 0);
+
+  jint retVal = csoundEvalCode(csound, nativeString);
+
+  env->ReleaseStringUTFChars(code, nativeString);
+
+  return retVal;
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundCompile
+ * Signature: (J[Ljava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundCompile
+(JNIEnv *env, jclass clazz, jlong csoundPtr, jobjectArray args) {
+  const char *argv[256]; //up to 256 commandline args...
+  CSOUND* csound = (CSOUND*)csoundPtr;
+  int argc = env->GetArrayLength(args);
+
+  for(int i = 0; i < argc; i++) {
+    jstring string = (jstring) (env->GetObjectArrayElement(args, i));
+    argv[i] = env->GetStringUTFChars(string, 0);
   }
+
+  int retVal = csoundCompile(csound, argc, argv);
+
+  // cleanup java strings
+  for(int i = 0; i < argc; i++) {
+    jstring string = (jstring) (env->GetObjectArrayElement(args, i));
+    env->ReleaseStringUTFChars(string, argv[i]);
+  }
+
+  return retVal;
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundCompileOrc
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundCompileOrc
+(JNIEnv *env, jclass clazz, jlong csoundPtr, jstring orcCode) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  const char *nativeString = env->GetStringUTFChars(orcCode, 0);
+
+  jint retVal = csoundCompileOrc(csound, nativeString);
+
+  env->ReleaseStringUTFChars(orcCode, nativeString);
+
+  return retVal;
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundCompileOrcAsync
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundCompileOrcAsync
+(JNIEnv *env, jclass clazz, jlong csoundPtr, jstring orcCode) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  const char *nativeString = env->GetStringUTFChars(orcCode, 0);
+
+  jint retVal = csoundCompileOrcAsync(csound, nativeString);
+
+  env->ReleaseStringUTFChars(orcCode, nativeString);
+
+  return retVal;
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundCompileCsdText
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundCompileCsdText
+(JNIEnv *env, jclass clazz, jlong csoundPtr, jstring csdText) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  const char *nativeString = env->GetStringUTFChars(csdText, 0);
+
+  jint retVal = csoundCompileOrcAsync(csound, nativeString);
+
+  env->ReleaseStringUTFChars(csdText, nativeString);
+
+  return retVal;
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundGetScoreTime
+ * Signature: (J)D
+ */
+JNIEXPORT jdouble JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundGetScoreTime
+(JNIEnv *env, jclass clazz, jlong csoundPtr) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  return csoundGetScoreTime(csound);
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundInputMessage
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT void JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundInputMessage
+(JNIEnv *env, jclass clazz, jlong csoundPtr, jstring scoreMessage) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  const char *nativeString = env->GetStringUTFChars(scoreMessage, 0);
+
+  csoundInputMessage(csound, nativeString);
+
+  env->ReleaseStringUTFChars(scoreMessage, nativeString);
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundInputMessageAsync
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT void JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundInputMessageAsync
+(JNIEnv *env, jclass clazz, jlong csoundPtr, jstring scoreMessage) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  const char *nativeString = env->GetStringUTFChars(scoreMessage, 0);
+
+  csoundInputMessageAsync(csound, nativeString);
+
+  env->ReleaseStringUTFChars(scoreMessage, nativeString);
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundReadScore
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundReadScore
+(JNIEnv *env, jclass clazz, jlong csoundPtr, jstring message) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  const char *nativeString = env->GetStringUTFChars(message, 0);
+
+  jint retVal = csoundReadScore(csound, nativeString);
+
+  env->ReleaseStringUTFChars(message, nativeString);
+
+  return retVal;
+}
+
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundReadScoreAsync
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT void JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundReadScoreAsync
+(JNIEnv *env, jclass clazz, jlong csoundPtr, jstring message) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  const char *nativeString = env->GetStringUTFChars(message, 0);
+
+  csoundReadScoreAsync(csound, nativeString);
+
+  env->ReleaseStringUTFChars(message, nativeString);
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundStart
+ * Signature: (J)V
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundStart
+(JNIEnv *env, jclass clazz, jlong csoundPtr) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  jint retVal = csoundStart(csound);
+  return retVal;
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundStop
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundStop
+(JNIEnv *env, jclass clazz, jlong csoundPtr) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  csoundStop(csound);
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundPerformKsmps
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundPerformKsmps
+(JNIEnv *env, jclass clazz, jlong csoundPtr) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  jint retVal = csoundPerformKsmps(csound);
+  return retVal;
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundCleanup
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundCleanup
+(JNIEnv *env, jclass clazz, jlong csoundPtr) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  jint retVal = csoundCleanup(csound);
+  return retVal;
+}
 
 /*
  * Class:     com_kunstmusik_jcsound_JCsound
  * Method:    csoundReset
- * Signature: (J)J
+ * Signature: (J)V
  */
-JNIEXPORT jlong JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundReset
-  (JNIEnv *env, jobject obj, jlong csoundPtr) {
-    std::cout << "Csound Reset Called." << std::endl;
-    csoundReset((CSOUND*)csoundPtr);
-    return 1; 
-  }
+JNIEXPORT void JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundReset
+(JNIEnv *env, jclass clazz, jlong csoundPtr) {
+  csoundReset((CSOUND*)csoundPtr);
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundGetSr
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundGetSr
+(JNIEnv *env, jclass clazz, jlong csoundPtr) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  jint retVal = csoundGetSr(csound);
+  return retVal;
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundGetKr
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundGetKr
+(JNIEnv *env, jclass clazz, jlong csoundPtr) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  jint retVal = csoundGetKr(csound);
+  return retVal;
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundGetKsmps
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundGetKsmps
+(JNIEnv *env, jclass clazz, jlong csoundPtr) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  jint retVal = csoundGetKsmps(csound);
+  return retVal;
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundGetNchnls
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundGetNchnls
+(JNIEnv *env, jclass clazz, jlong csoundPtr) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  jint retVal = csoundGetNchnls(csound);
+  return retVal;
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundGetNchnlsInput
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundGetNchnlsInput
+(JNIEnv *env, jclass clazz, jlong csoundPtr) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  jint retVal = csoundGetNchnlsInput(csound);
+  return retVal;
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundGet0dBFS
+ * Signature: (J)D
+ */
+JNIEXPORT jdouble JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundGet0dBFS
+(JNIEnv *env, jclass clazz, jlong csoundPtr) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+
+  jdouble retVal = csoundGet0dBFS(csound);
+  return retVal;
+}
+
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundSetControlChannel
+ * Signature: (JLjava/lang/String;D)V
+ */
+JNIEXPORT void JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundSetControlChannel
+(JNIEnv *env, jclass clazz, jlong csoundPtr, jstring channel, jdouble value) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+  const char *nativeString = env->GetStringUTFChars(channel, 0);
+
+  csoundSetControlChannel(csound, nativeString, value);
+
+  env->ReleaseStringUTFChars(channel, nativeString);
+}
+/*
+ * Class:     com_kunstmusik_jcsound_JCsound
+ * Method:    csoundSetStringChannel
+ * Signature: (JLjava/lang/String;Ljava/lang/String;)V
+ */
+JNIEXPORT void JNICALL Java_com_kunstmusik_jcsound_JCsound_csoundSetStringChannel
+(JNIEnv *env, jclass clazz, jlong csoundPtr, jstring channel, jstring value) {
+  CSOUND* csound = (CSOUND*)csoundPtr;
+  const char *chanName = env->GetStringUTFChars(channel, 0);
+  const char *chanValue = env->GetStringUTFChars(value, 0);
+
+  csoundSetStringChannel(csound, chanName, (char*)chanValue);
+
+  env->ReleaseStringUTFChars(channel, chanName);
+  env->ReleaseStringUTFChars(value, chanValue);
+}
 
--- src/JCsound.java
+++ src/JCsound.java
@@ -1,7 +1,80 @@
 package com.kunstmusik.jcsound;
 
 public class JCsound {
+    static {
+      System.loadLibrary("jcsound");
+    }
 
-    public native long csoundCreate(int initFlags);
-    public native long csoundReset(long csoundRef);
+    public static native long csoundCreate();
+    public static native int csoundGetVersion(); 
+    public static native int csoundSetOption(long csoundPtr, String option);
+
+    public static native int csoundEvalCode(long csoundPtr, String s);     
+    public static native int csoundCompile(long csoundPtr, String[] args);
+
+    public static native int csoundCompileOrc(long csoundPtr, String s); 
+
+    public static native int csoundCompileOrcAsync(long csoundPtr, String s); 
+
+    public static native int csoundCompileCsdText(long csoundPtr, String csdText);     
+
+    public static native double csoundGetScoreTime(long csoundPtr); 
+
+    public static native void csoundInputMessage(long csoundPtr, String s); 
+
+    public static native void csoundInputMessageAsync(long csoundPtr, String s); 
+    public static native int csoundReadScore(long csoundPtr, String s); 
+    public static native void csoundReadScoreAsync(long csoundPtr, String s); 
+
+    public static native int csoundStart(long csoundPtr); 
+
+    public static native void csoundStop(long csoundPtr);
+
+    public static native int csoundPerformKsmps(long csoundPtr); 
+    public static native int csoundCleanup(long csoundPtr); 
+
+    public static native void csoundReset(long csoundPtr); 
+
+    //public void setMessageCallback(MessageCallback cb) {
+    //    csoundSetMessageStringCallback(csoundPtr, cb);
+    //}
+
+    public static native int csoundGetSr(long csoundPtr); 
+
+    public static native int csoundGetKr(long csoundPtr);
+
+    public static native int csoundGetKsmps(long csoundPtr);
+
+    public static native int csoundGetNchnls(long csoundPtr);
+
+    public static native int csoundGetNchnlsInput(long csoundPtr);
+
+    public static native double csoundGet0dBFS(long csoundPtr);
+
+    //public DoubleBuffer getSpin(){
+        //Pointer p = csoundGetSpin(csoundPtr);
+        //int nchnls_i = csoundGetNchnlsInput(csoundPtr);
+        //int ksmps = csoundGetKsmps(csoundPtr);
+        //// 8 since double is 8 bytes in size
+        //ByteBuffer b = p.getByteBuffer(0, nchnls_i * ksmps * 8);
+        //return b.asDoubleBuffer();
+    //}
+
+    //public DoubleBuffer getSpout(){
+        //Pointer p = csoundGetSpout(csoundPtr);
+        //int nchnls = csoundGetNchnls(csoundPtr);
+        //int ksmps = csoundGetKsmps(csoundPtr);
+
+        //// 8 since double is 8 bytes in size
+        //ByteBuffer b = p.getByteBuffer(0, nchnls * ksmps * 8);
+        //return b.asDoubleBuffer();
+    //}
+    
+    public static native void csoundSetControlChannel(long csoundPtr, String name, double value);
+
+    public static native void csoundSetStringChannel(long csoundPtr, String channelName, String channelValue);
+
+    //public void getChannelPtr(Pointer p, String name, int type) {
+        //csoundGetChannelPtr(csoundPtr, p, name, type);
+    //}
 }
